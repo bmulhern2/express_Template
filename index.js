b74c9db743b97c19deed673c02ef46b11d25bc6b
bmulhern2 <bmulhern09@gmail.com>
2019-12-30 14:03:50 -0500

    commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+// Connect To Database
+mongoose.connect('mongodb+srv://bmulhern2:<password>@cluster0-rsgdt.mongodb.net/test?retryWrites=true&w=majority');
+// Define Schema
+// Connect Schema to Form
+// Initalize App
 var app = express();
+// Route the Index.html file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+// Listen on port 3000
 app.listen(3000);
+// Exports App
 module.exports = app;
